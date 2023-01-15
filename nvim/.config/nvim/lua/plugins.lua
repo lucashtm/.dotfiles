b9ac801fba664e30910875e9b1ec6aa0b47563a4
Lucas Monteiro <lucashtm@id.uff.br>
2023-01-15 18:44:33 +0000

    Adds a bunch of plugins to neovim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,5 +1,21 @@
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'neovim/nvim-lspconfig'
-  use 'preservim/nerdtree'
+  use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' }}
+  use 'BurntSushi/ripgrep'
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+  use({
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+      vim.cmd('colorscheme rose-pine')
+    end
+  })
+  use 'ThePrimeagen/harpoon'
+  use 'mbbill/undotree'
+  use 'tpope/vim-fugitive'
 end)
