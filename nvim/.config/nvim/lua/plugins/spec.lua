7737a714dcf7fa4786de4e8cc5e0ad342f8b1d27
Lucas Monteiro <lucas_monteiro@apple.com>
2025-05-20 12:35:46 -0300

    Fix neovim lsp

--- nvim/.config/nvim/lua/plugins/spec.lua
+++ nvim/.config/nvim/lua/plugins/spec.lua
@@ -1,7 +1,7 @@
 return {
   -- LSP Support
-  { "williamboman/mason.nvim" },
-  { "williamboman/mason-lspconfig.nvim" },
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
   { "neovim/nvim-lspconfig" },
   -- Autocompletion
   {
